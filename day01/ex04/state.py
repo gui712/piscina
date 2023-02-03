c879e134041beb661560c4e74d08fc1c2f072ccb
Guilherme Costa Silva <guicosta@c2r2p5.42sp.org.br>
2023-02-03 16:06:53 -0300

    Ex04 Ok

---
+++ day01/ex04/state.py
@@ -0,0 +1,32 @@
+import sys
+def listCities(argv):
+    states = {
+    "Oregon" : "OR",
+    "Alabama" : "AL",
+    "New Jersey": "NJ",
+    "Colorado" : "CO"
+    }
+    capital_cities = {
+    "OR": "Salem",
+    "AL": "Montgomery",
+    "NJ": "Trenton",
+    "CO": "Denver"
+    }
+
+    if not(argv[1] in capital_cities.values()):
+        print("Unknown capital city")
+        sys.exit()
+
+    for key, value in capital_cities.items():
+        if(argv[1] == value):
+            new_value = key
+
+    for key, value in states.items():
+        if(new_value == value):
+            print(key)
+
+
+if __name__ == '__main__':
+    if(len(sys.argv) != 2):
+        sys.exit()
+    listCities(sys.argv)

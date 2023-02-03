35d72fecf918ad00937d5ef28a2201cf02e6c35c
Guilherme Costa Silva <guicosta@c2r2p5.42sp.org.br>
2023-02-03 12:21:17 -0300

    Ex03 Okay

---
+++ day01/ex03/capital_city.py
@@ -0,0 +1,30 @@
+import sys
+
+def listCapitals(argv):
+    states = {
+    "Oregon" : "OR",
+    "Alabama" : "AL",
+    "New Jersey": "NJ",
+    "Colorado" : "CO"
+    }
+    capital_cities = {
+    "OR": "Salem",
+    "AL": "Montgomery",
+    "NJ": "Trenton",
+    "CO": "Denver"
+    }
+
+    if not(argv[1] in states):
+        print("Unknown state")
+        sys.exit()
+    print(capital_cities[states[argv[1]]])
+
+
+
+
+
+
+if __name__ == '__main__':
+    if(len(sys.argv) != 2):
+        sys.exit()
+    listCapitals(sys.argv)

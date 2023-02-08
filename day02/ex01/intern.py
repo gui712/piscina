f5d2e4858157317f9f47cd64d07c270bc9d19467
Guilherme Costa Silva <guicosta@c2r2p5.42sp.org.br>
2023-02-08 17:47:21 -0300

    day02 ex01

---
+++ day02/ex01/intern.py
@@ -0,0 +1,21 @@
+class Intern:
+    def __init__(self,name = "My name? I'm nobody, an intern, I have no name."):
+        self.name = name
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+    class Coffee:
+             def __str__(self):
+                return f'This is the worst coffee you ever tasted.'
+
+    def work(self)
+        return                 
+
+
+if __name__ == '__main__':
+    p1 = Intern('Gui')
+    p2 = Intern.Coffee()
+    print(p1)
+    print(p2)
